0cc909457a97fbe99f71a265590ec99a8d21c964
Julianne Waber <juliannewaber@gmail.com>
2020-08-16 19:10:53 -0400

    level 1

---
+++ objects/DoorExit.gd
@@ -0,0 +1,24 @@
+extends StaticBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+export var opened = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$DoorClosed.show()
+	$CollisionShape2D.set_deferred("enabled", true)
+	opened = false
+
+func open():
+	$DoorClosed.hide()
+	$CollisionShape2D.set_deferred("disabled", true)
+	opened = true
+	$DoorOpen.show()
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
