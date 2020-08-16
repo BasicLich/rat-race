ea79a05604026465f5a3f72ae88b7c7319c52ea9
Henry Webster <hwebs@hwebs.info>
2020-08-16 15:48:29 -0400

    rat reacts to death, hands come out

--- Arrow.gd
+++ Arrow.gd
@@ -2,10 +2,13 @@ extends Area2D
 
 export var speed = 50
 
+signal hit()
+
 func _process(delta):
 	position -= Vector2(speed * delta, 0.0)
 	
 func _on_Arrow_body_entered(body):
-	print("hit!")
+	if body.is_in_group("player"):
+		body.kill()
 	if !body.is_in_group("shooter"):
 		queue_free()
